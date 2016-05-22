74a874649081b8f2b2b01d7c3c20a08b4ae64d32
sbiju <sekharanbijumon@gmail.com>
2016-05-22 11:13:24 +0530

    Modified url

--- mysite/urls.py
+++ mysite/urls.py
@@ -22,7 +22,8 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
-     url(r'^posts/', include('posts.urls', namespace='posts')),
+    url(r'^posts/', include('posts.urls', namespace='posts')),
+    url(r'^', include('posts.urls', namespace='posts')),
 ]
 if settings.DEBUG:
     urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
